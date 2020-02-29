750e47923087cb81f259121e25e946a6bbe62483
kpenov16 <s133967@student.dtu.dk>
2020-02-29 13:42:10 +0100

    - fix message null pointer

--- src/dk/workers/Envelope.java
+++ src/dk/workers/Envelope.java
@@ -34,8 +34,7 @@ public class Envelope {
 	/* Get message. We must escape the message to make sure that
 	   there are no single periods on a line. This would mess up
 	   sending the mail. */
-        //kpv, hardcode for now
-	    //Message = escapeMessage(message);
+        Message = escapeMessage(message);
 
         /* Take the name of the local mailserver and map it into an
          * InetAddress */
